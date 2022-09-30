0680cc6b86bbf1b3c320971b8395accde46a2c3a
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-09-30 19:54:59 -0300

    Adding Classes

---
+++ LanchesDoTafa/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LanchesDoTafa.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesDoTafa.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+    }
+}
