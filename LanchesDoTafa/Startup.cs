5bf72c7a31b69494579b3a9c9c9c037fd8c70456
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-09-30 20:30:19 -0300

    Add string conexion

--- LanchesDoTafa/Startup.cs
+++ LanchesDoTafa/Startup.cs
@@ -1,4 +1,7 @@
-﻿namespace LanchesDoTafa;
+﻿using LanchesDoTafa.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesDoTafa;
 public class Startup
 {
     public Startup(IConfiguration configuration)
@@ -11,6 +14,8 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaulConnection")));
         services.AddControllersWithViews();
     }
 
