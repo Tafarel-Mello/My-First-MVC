d169ab442fefdb5eb0f79bb887bf96e380cc7d2f
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-09-30 19:27:01 -0300

    First Commit
    Starup project

---
+++ LanchesDoTafa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LanchesDoTafa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
