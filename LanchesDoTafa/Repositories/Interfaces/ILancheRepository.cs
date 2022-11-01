b100132a8b7539c2051773f56232279e554fbf03
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-11-01 20:03:22 -0300

    Commit
    Feat: Add interfaces and repositories

---
+++ LanchesDoTafa/Repositories/Interfaces/ILancheRepository.cs
@@ -0,0 +1,11 @@
+﻿using LanchesDoTafa.Models;
+
+namespace LanchesDoTafa.Repositories.Interfaces
+{
+    public interface ILancheRepository
+    {
+        IEnumerable<Lanche> Lanches { get; }
+        IEnumerable<Lanche> LanchesPreferidos { get; }
+        Lanche GetLancheById(int lancheId);
+    }
+}
