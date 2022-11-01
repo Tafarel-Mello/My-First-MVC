631c087ad6c69aac735dda190808dc074ca9f983
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-11-01 19:34:42 -0300

    Commit
    Feat: Add Data in data-base with MIgrations

---
+++ LanchesDoTafa/Migrations/20221101221829_PopularCategorias.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesDoTafa.Migrations
+{
+    public partial class PopularCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias (CategoriaName, Descricao)" +
+                "VALUES ('Normal', 'Lanche feito com ingredientes normais')");
+
+            migrationBuilder.Sql("INSERT INTO Categorias (CategoriaName, Descricao)" +
+                 "VALUES ('Natural', 'Lanche feito com ingredientes naturais e organicos')");
+
+            migrationBuilder.Sql("INSERT INTO Categorias (CategoriaName, Descricao)" +
+                 "VALUES ('Sobremesa', 'Sobremesas caseiras')");
+
+            migrationBuilder.Sql("INSERT INTO Categorias (CategoriaName, Descricao)" +
+                "VALUES ('Bebidas', 'Sucos, refrigerantes, agua, cervejas')");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorais");
+        }
+    }
+}
