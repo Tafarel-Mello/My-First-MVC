a55c72f3b71628a10ce5e7da15a9537a371c5a07
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-10-31 22:23:45 -0300

    Commit
    Feat: Update

---
+++ LanchesDoTafa/Controllers/LancheController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesDoTafa.Controllers
+{
+    public class LancheController : Controller
+    {
+
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
