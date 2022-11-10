556e96311de52773daba6c7661b320b415ccf6ac
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-11-10 00:04:19 -0300

    Commit
    Feat: improving model carrinho

---
+++ LanchesDoTafa/Models/CarrinhoCompra.cs
@@ -0,0 +1,89 @@
+﻿using LanchesDoTafa.Context;
+
+namespace LanchesDoTafa.Models
+{
+    public class CarrinhoCompra
+    {
+        private readonly AppDbContext _context;
+
+        public CarrinhoCompra(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        public string CarrinhoCompraId { get; set; }
+        public List<CarrinhoCompraItem> CarrinhoCompraItems { get; set; }
+
+
+        // metodos para a session no sistema ------>
+        public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+        {
+            // definir a sessao
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            // obtem um servico do tipo do nosso contexto
+            var context = services.GetService<AppDbContext>();
+
+            // obtem ou gera um Id do carrinho
+            string carrinhoId = session.GetString("CarrinhoId")??Guid.NewGuid().ToString();
+
+            // atribui o Id do carrinho na sessao
+            session.SetString("CarrinhoId", carrinhoId);
+
+            //retorna o carrinho com o contexto e o Id atribuido ou obtido
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId
+            };
+
+        }
+
+        // metodo para adicionar item ao carrinho
+        public void AdicionarAoCarrinho(Lanche lanche)
+        {
+            var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(s =>
+                 s.Lanche.LancheId == lanche.LancheId && s.CarrinhoCompraId == CarrinhoCompraId);
+
+            if (carrinhoCompraItem == null)
+            {
+                carrinhoCompraItem = new CarrinhoCompraItem
+                {
+                    CarrinhoCompraId = CarrinhoCompraId,
+                    Lanche = lanche,
+                    Quantidade = 1
+                };
+                _context.CarrinhoCompraItens.Add(carrinhoCompraItem);
+            }
+            else
+            {
+                carrinhoCompraItem.Quantidade++;
+            }
+            _context.SaveChanges();
+        }
+
+        // metodo para remover do carrinho
+
+        public int RemoverDoCarrinho(Lanche lanche)
+        {
+            var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(s =>
+                s.Lanche.LancheId == lanche.LancheId && s.CarrinhoCompraId == CarrinhoCompraId);
+
+            var quantidadeLocal = 0;
+
+            if (carrinhoCompraItem != null)
+            {
+                if (carrinhoCompraItem.Quantidade > 1)
+                {
+                    carrinhoCompraItem.Quantidade--;
+                    quantidadeLocal = carrinhoCompraItem.Quantidade;
+                }
+                else
+                {
+                    _context.CarrinhoCompraItens.Remove(carrinhoCompraItem);
+                }
+            }
+            _context.SaveChanges();
+            return quantidadeLocal;
+        }
+    }
+}
